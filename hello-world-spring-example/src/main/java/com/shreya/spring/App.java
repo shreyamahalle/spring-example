69af5de55829ec635e3a0a2c0b5ddc900299cc3b
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-11 16:06:58 +0530

    changes added

--- hello-world-spring-example/src/main/java/com/shreya/spring/App.java
+++ hello-world-spring-example/src/main/java/com/shreya/spring/App.java
@@ -3,9 +3,6 @@ import com.shreya.spring.service.HelloService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
 //        HelloService helloService = new HelloService();
