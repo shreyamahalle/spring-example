b284bfeddf3cbf42519e9ca36773bf54a1d53fc8
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-22 12:54:20 +0530

    changes added

--- spring-annotation-demo/src/main/java/com/shreya/spring/App.java
+++ spring-annotation-demo/src/main/java/com/shreya/spring/App.java
@@ -1,17 +1,14 @@
 package com.shreya.spring;
 
+import com.shreya.spring.config.ConfigApp;
 import com.shreya.spring.model.Address;
 import com.shreya.spring.model.City;
 import com.shreya.spring.service.HelloService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "com.shreya.spring")
 
 public class App {
+
     public static ApplicationContext context;
     public static void main( String[] args ) {
 
@@ -19,12 +16,11 @@ public class App {
 
         Address address = Address.builder().city(city).build();
 
-        context = new AnnotationConfigApplicationContext(App.class);
+        context = new AnnotationConfigApplicationContext(ConfigApp.class);
         HelloService helloService = context.getBean("helloService" , HelloService.class);
         helloService.setName("shreya");
         helloService.setAddress(address);
 
         helloService.sayHello();
-
     }
 }
--- spring-annotation-demo/src/main/java/com/shreya/spring/repository/HelloRepository.java
+++ spring-annotation-demo/src/main/java/com/shreya/spring/repository/HelloRepository.java
@@ -3,6 +3,10 @@ package com.shreya.spring.repository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+
 public class HelloRepository {
-    public  void
+
+    public String greet() {
+        return "Hello world";
+    }
 }
