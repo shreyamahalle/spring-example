068178ce2a745c135d54128cd857e45232e7f629
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-23 13:01:27 +0530

    xml file added

--- spring-demo-practice/src/main/java/com/shreya/spring/App.java
+++ spring-demo-practice/src/main/java/com/shreya/spring/App.java
@@ -1,13 +1,9 @@
 package com.shreya.spring;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
     }
 }
--- spring-demo-practice/src/main/java/com/shreya/spring/service/BookingService.java
+++ spring-demo-practice/src/main/java/com/shreya/spring/service/BookingService.java
@@ -1,13 +1,20 @@
 package com.shreya.spring.service;
 
+import com.shreya.spring.model.Booking;
 import com.shreya.spring.repository.BookingRepositroy;
+import lombok.Data;
 
+@Data
 public class BookingService {
 
    private BookingRepositroy bookingRepositroy;
+   private Booking booking;
 
    public void createBopking(){
 
+   }
+   public void printBooking(){
+      System.out.println("Booking details " + booking);
    }
 
 }
